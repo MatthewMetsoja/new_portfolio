d973b398151c09603b45f43815e0a345f2acd218
Matthew Metsoja <matthewmetsoja1@icloud.com>
2020-10-24 23:39:46 +0100

    finished desktop header

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,5 @@
 const smokeVid = document.getElementById("smokeVideo");
+const fullName = document.getElementById("fullName");
 
 setTimeout( () => {
     
@@ -10,6 +11,18 @@ setTimeout( () => {
 setTimeout( () => {
     // const smokeVid = document.getElementById("smokeVideo");
     smokeVid.style.opacity = 0;
+    
+}, 7000);
+
+setTimeout( () => {
+    // const smokeVid = document.getElementById("smokeVideo");
+    smokeVid.style.display = 'none';
+    
+}, 8000);
+
+setTimeout( () => {
+    // const smokeVid = document.getElementById("smokeVideo");
+    fullName.classList.add('fullName');
     
 }, 7000);
 
